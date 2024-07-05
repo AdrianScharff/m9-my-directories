0a69cf860cbacae743eeaa32af7cca20eca9741c
AdrianScharff <adrian18juarez@gmail.com>
2024-07-05 11:20:10 -0600

    set header, register and login

--- 03-frontend-class/src/App.jsx
+++ 03-frontend-class/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Header from './components/Header'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Router>
         <div className="container">
+          <Header/>
           <Routes>
             <Route path='/' element={<Dashboard/>}/>
             <Route path='/login' element={<Login/>}/>
--- 03-frontend-class/src/pages/Login.jsx
+++ 03-frontend-class/src/pages/Login.jsx
@@ -1,7 +1,68 @@
+import { useState, useEffect } from "react"
+import { FaSignInAlt} from 'react-icons/fa'
 
 const Login = () => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }) )
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    // iria la funcionalidad de crear un usuario
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <section className="heading">
+        <h4>
+          <FaSignInAlt /> Login
+        </h4>
+        <p>
+          Por favor teclea tus credenciales
+        </p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type='email'
+              className="form-control"
+              id='email'
+              name='email'
+              value={email}
+              placeholder="Por favor teclea tu email"
+              onChange={onChange}
+            />
+            <input
+              type='password'
+              className="form-control"
+              id='password'
+              name='password'
+              value={password}
+              placeholder="Por favor teclea tu password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type='submit' className="btn btn-block">
+              Ingresar
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
