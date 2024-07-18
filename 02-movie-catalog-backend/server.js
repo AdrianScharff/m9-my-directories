d160b7534150ba1a5231e15c971a9f7e0f657b10
AdrianScharff <adrian18juarez@gmail.com>
2024-07-18 11:08:36 -0600

    Add cors library to avoid cors errors

--- 02-movie-catalog-backend/server.js
+++ 02-movie-catalog-backend/server.js
@@ -5,12 +5,15 @@ const movieRoutes = require('./routes/movieRoutes')
 const userRoutes = require('./routes/userRoutes')
 const connectDB = require('./config/db')
 const { errorHandler } = require('./middlewares/errorHandler')
+const cors = require('cors')
 
 connectDB()
 
 const port = process.env.PORT || 5000
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
